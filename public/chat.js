dcbfea925a2aafd9769bbf7164bbd67a894dabcf
BasedBird <basedbird445@gmail.com>
2023-07-09 02:41:39 -0400

    Implemented delete

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,5 @@
+var currNode = null;
+
 if ( window.history.replaceState ) {
   window.history.replaceState( null, null, window.location.href );
 }
@@ -17,9 +19,36 @@ webSocket.addEventListener("message", (event) => {
     window.location.replace("./");
   }
   else{
+    var code = event.data.split(' ')[0];
+    var data = event.data.slice(code.length+1);
+    switch(code) {
+      case '304':
+        var id = data.split(' ')[0];
+        var parent = document.getElementById('chat');
+        var newDiv = document.createElement('div');
+        var newContent = document.createTextNode(data.slice(id.length+1));
+        var newButton = document.createElement('button');
+        newButton.type = "button";
+        newButton.innerHTML = "Delete";
+        newButton.addEventListener('click', function(e) {
+          webSocket.send('305 ' + e.target.parentNode.id);
+        });
+
+        newDiv.setAttribute('id', id);
+        newDiv.appendChild(newContent);
+        newDiv.appendChild(newButton);
+        parent.appendChild(newDiv);
+        //document.getElementById("chat").innerHTML += '<br>' + event.data;
+        document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+        break;
+      case '305':
+        document.getElementById(data).innerHTML = "deleted";
+        break;
+      default:
+    }
     //document.body.innerHTML = document.body.innerHTML + '<br>' + event.data;
-    document.getElementById("chat").innerHTML += '<br>' + event.data;
-    document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+    //document.getElementById("chat").innerHTML += '<div id=\'' + id '\'>' + event.data.slice(id.length) + '</div>';
+    //document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
   }
 });
 
@@ -38,9 +67,13 @@ function bye_button(){
 }
 
 function message_button(){
-  webSocket.send('202 ' + document.getElementById('message').value);
+  webSocket.send('304 ' + document.getElementById('message').value);
   document.getElementById("message").value = '';
   document.getElementById("message").focus();
+}
+
+function delete_button(e){
+  webSocket.send('111 ' + e.target.id);
 }
 
 function checkSignedIn(){
