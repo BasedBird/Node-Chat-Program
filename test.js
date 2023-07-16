e14fffa3a2cdad4c1e4a5c88ff20353e3196f8c9
BasedBird <basedbird445@gmail.com>
2023-07-16 15:11:05 -0400

    include gitignore
    also deleted ignore files from main branch

--- test.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const sql_driver = require('./MysqlDriver');
-
-sql_driver.fetchLatest(function(res){
-  for (let i = 0; i<5; i++){
-    console.log(res[i][0], res[i][1]);
-  }
-})
